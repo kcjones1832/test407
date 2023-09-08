4aa2184fe540a36aa34bbc0dd006908e9a888da7
kcjones1832 <caseyhunterjones@gmail.com>
2023-09-08 12:22:14 -0500

    added hello world

---
+++ hello_world.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class hello_world{ 
+    public static void main(String[] args) {
+        System.out.println("hello world");
+        Scanner scnr = new Scanner(System.in);
+        System.out.println(scnr.nextLine());
+        scnr.close();
+    }
+}
